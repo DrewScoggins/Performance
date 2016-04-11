eede3dbde1955a25ed97d87341d3825f6b97dfda
Peter Hsu <shhsu@microsoft.com>
2016-04-11 14:22:45 -0700

    Various script changes to accommodate shared framework. (Basc script not completed)

--- test/ColdStart/SetEnv.sh
+++ test/ColdStart/SetEnv.sh
@@ -13,17 +13,18 @@ if [ ! -d $outputDir ]; then
     mkdir -p $outputDir
 fi
 
-outputFile=`find $outputDir -maxdepth 1 | sort | tail -1`
+outputFile=`find $outputDir -maxdepth 1 -name *.csv | sort | tail -1`
 
-if [ ! -f $outputFile ]
+if [ -z "$outputFile" ] || [ ! -f $outputFile ]
 then
 ## There is no existing run, create a new run and use timestamp as runId
     runId=`date +%y-%m-%d-%H-%M-%S`
     outputFile="${outputDir}/${runId}.csv"
+else
+    echo "Output file ${outputFile} exists, results will be appended to it."
 fi
 
 echo "Tests will be recorded in file: $outputFile"
 
 export outputFile
 
-export DOTNET_PACKAGES_CACHE="${HOME}/.nuget/packages"
